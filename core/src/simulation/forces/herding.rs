0d25703d30af7c9b6ac8339466a7fea8db84fdfc
azhi <astzhe@gmail.com>
2017-06-02 02:34:50 +0300

    normalize herding force

--- core/src/simulation/forces/herding.rs
+++ core/src/simulation/forces/herding.rs
@@ -13,13 +13,20 @@ impl Forceable for HerdingForce {
         const HERDING_FOV_DISTANCE: f64 = 3f64;
 
         let mut force = Vector::zero();
+        let mut n = 0;
 
         if person.panic_level != 0_f64 {
             for other_person in scene.people.iter() {
                 if (person.coordinates - other_person.coordinates).length() * scene.scale < HERDING_FOV_DISTANCE {
                     force = force + Vector::new(other_person.heading.cos(), other_person.heading.sin()) * other_person.forces_params.target_speed;
+
+                    n += 1;
                 }
             }
+        }
+
+        if n != 0 {
+            force = force / (n as f64);
         }
 
         force = force * person.panic_level * person.forces_params.herding;
